7204f5b14ae2f97a5beceac83cac2b32af2a4c40
AndriiMytiev <andrijmitev95@gmail.com>
2025-05-14 14:15:54 +0300

    update main.tf

--- main.tf
+++ main.tf
@@ -69,6 +69,17 @@ usermod -aG docker ec2-user
 # Витягування та запуск контейнера
 docker pull andriimytiev/mzuiit-lab2:latest
 docker run -d -p 80:80 andriimytiev/mzuiit-lab2:latest
+
+# Запуск Watchtower з --cleanup
+docker run -d \
+  --name watchtower \
+  --restart always \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  containrrr/watchtower \
+  --interval 30 \
+  --cleanup \
+  mzuiit-lab2
+
 EOF
 }
 
